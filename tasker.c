5b8a3e4f874997f2a1cda5f0472346a4d69d0e54
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-11-12 20:08:09 +0700

    fgets

--- tasker.c
+++ tasker.c
@@ -30,17 +30,24 @@ char *input_name() {
    char buff[20];
 
    printf("Enter name = ");
-   scanf("%19s", buff);
+   while ((getchar()) != '\n');
+   
+   if (fgets(buff, sizeof(buff), stdin) == NULL) {
+      printf("Out of stream\n");
+      exit(1);
+   }
 
    str = malloc(sizeof(buff));
 
    if (str == NULL) {
       printf("Out of memory\n");
       exit(1);
+   } else {
+      buff[strlen(buff) - 1] = '\0';
    }
 
    strcpy(str, buff);
-
+   
    return str;
 }
 
